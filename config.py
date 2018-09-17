54533fd9bf24d71f0e21b95615e5ceaf5db84dcd
zhongyuchen <izhongyuchen@gmail.com>
2018-09-17 22:01:58 +0800

    finish model

---
+++ config.py
@@ -0,0 +1,77 @@
+"""ChessWarrior configuration"""
+
+import os
+
+# -----------------------------------
+# configurations about some PARAMETERS
+# -----------------------------------
+
+class ResourceConfig(object):
+    """resouces configuratioins"""
+    cur_dir = os.path.abspath(__file__)
+
+    d = os.path.dirname
+    base_dir = d(d(cur_dir))
+
+    _base_data_dir = os.path.join(base_dir, "data")
+
+    best_model_dir = os.path.join(_base_data_dir, "model")
+
+    _sl_base_data_dir = os.path.join(_base_data_dir, "human_expert")
+
+    sl_raw_data_dir = os.path.join(_sl_base_data_dir, "raw") #pgn files
+
+    sl_processed_data_dir = os.path.join(_sl_base_data_dir, "processed") #json files
+
+    value_data_dir = os.path.join(_base_data_dir, "tmp")
+
+    json_size = 1024 #moves in a json file
+
+    min_elo = 600.0 #min_elo weight = 0
+    max_elo = 2350.0 #max_elo weight = 1
+
+class ModelConfig(object):
+    """Model Configuration"""
+    cnn_filter_num = 256
+    res_layer_num = 39
+    cnn_first_filter_size= 5
+    cnn_filter_size = 3
+    l2_regularizer = 1e-5
+    value_fc_size = 256
+    drop_out_rate = 0.5
+    features = 18
+
+
+class TrainerConfig(object):
+    """Training Configuration"""
+    batch_size = 512
+    learning_rate = 0.01
+    epoches = 100
+    loss_weights = [1.25, 1.0]
+    save_interval = 1
+    test_interval = 5
+
+
+class PlayerConfig(object):
+    """Playing Configuration"""
+    cur_dir = os.path.abspath(__file__)
+    d = os.path.dirname
+    base_dir = d(d(cur_dir))
+
+    oppo_move_dir = os.path.join(base_dir, "oppo_move.txt")
+    ai_move_dir = os.path.join(base_dir, "ai_move.txt")
+    
+
+class Config(object):
+    """Configurations"""
+    CMD = ['train', 'play', 'data']
+
+    cuda_avaliable = True
+
+    resources = ResourceConfig()
+
+    model = ModelConfig()
+
+    training = TrainerConfig()
+
+    playing = PlayerConfig()
---
+++ model.py
@@ -0,0 +1,107 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class AlphaChess(nn.Module):
+    def __init__(self, config):
+        super(AlphaChess, self).__init__()
+        self.config = config
+        
+        self.init = nn.Sequential(
+            nn.Conv2d(
+                self.config.model.features,
+                self.config.model.cnn_filter_num,
+                self.config.model.cnn_first_filter_size,
+                padding=(self.config.model.cnn_first_filter_size // 2)
+            ),
+            nn.BatchNorm2d(self.config.model.cnn_filter_num),
+            nn.ReLU()
+        )
+
+        self.blocks = []
+        for i in range(self.config.model.res_layer_num):
+            self.blocks.append(ResidualBlock(
+                self.config.model.cnn_filter_num,
+                self.config.model.cnn_filter_num,
+                self.config.model.cnn_filter_size
+            ))
+        
+        self.res_layer = nn.Sequential(*self.blocks)
+
+        self.policy_head = nn.Sequential(
+            nn.Conv2d(
+                self.config.model.cnn_filter_num,
+                12,
+                1
+            ),
+            nn.BatchNorm2d(12),
+            nn.ReLU()
+        )
+                
+
+        self.value_head = nn.Sequential(
+            nn.Conv2d(
+                self.config.model.cnn_filter_num,
+                6,
+                1
+            ),
+            nn.BatchNorm2d(6),
+            nn.ReLU()
+        )
+
+        self.policy_linear = nn.Linear(8*8*12, 1968)
+
+        self.value_linear1 = nn.Linear(8*8*6, 256)
+        self.value_linear2 = nn.Linear(256, 1)
+
+    def forward(self, x):
+        s = self.init(x)
+        s = self.res_layer(s)
+
+        pi = self.policy_head(s)
+        pi = self.policy_linear(pi.view(-1, 8*8*12))
+
+        V = self.value_head(s)
+        V = self.value_linear1(V.view(-1, 8*8*6))
+        V = F.relu(V)
+        V = self.value_linear2(V)
+        V = torch.tanh(V)
+
+        return pi, V 
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel):
+        super(ResidualBlock, self).__init__()
+        self.relu = nn.ReLU(inplace=True)
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels,
+                      out_channels,
+                      kernel,
+                      padding=(kernel // 2)
+                      ),
+            nn.BatchNorm2d(out_channels),
+            self.relu,
+            nn.Conv2d(in_channels,
+                      out_channels,
+                      kernel,
+                      padding=(kernel // 2)
+                      ),
+            nn.BatchNorm2d(out_channels)
+        )
+    
+    def forward(self, x):
+        out = self.block(x)
+        residual = x
+        out += residual
+        return self.relu(out)
+    
+
+if __name__=='__main__':
+    x = torch.randn(32, 18, 8, 8)
+    from config import Config 
+    config = Config()
+    model = AlphaChess(config)
+    pi, V = model(x)
