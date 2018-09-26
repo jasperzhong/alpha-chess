ee1eef4a10e430c69679d3a32f77a9c18c3bcf8d
zhongyuchen <izhongyuchen@gmail.com>
2018-09-26 16:45:43 +0800

    wired

--- run_a3c.py
+++ run_a3c.py
@@ -3,7 +3,7 @@ import os
 
 import torch
 import torch.multiprocessing as mp
-from tensorboardX import SummaryWriter
+
 
 from alphachess.config import Config
 from alphachess.model import AlphaChess
@@ -63,9 +63,7 @@ if __name__=="__main__":
     
     step_counter = mp.Value('i', weights['step_num'])
     game_counter = mp.Value('i', weights['game_count'])
-    writer = SummaryWriter(log_dir="runs/A3C-lr{0}-gamma{1}-process{2}-model-resnet19-policy4-value2-valuefc256".format(
-        args.lr, args.gamma, args.num_processes
-    ))
+    
 
     lock = mp.Lock()
 
